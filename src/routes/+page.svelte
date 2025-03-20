92370662b5c34695d2112f3d1cf59c48f6c1d450
ihtfy <13478274+IHTFY@users.noreply.github.com>
2025-03-19 21:38:26 -0700

    blah

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,25 @@
 <script lang="ts">
+	import { ModeWatcher } from 'mode-watcher';
+	import '../app.css';
+
+	import Moon from 'lucide-svelte/icons/moon';
+	import Sun from 'lucide-svelte/icons/sun';
+
 	import Game from '$lib/components/Game.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import { toggleMode } from 'mode-watcher';
 </script>
+
+<ModeWatcher />
+
+<Button on:click={toggleMode} variant="outline" size="icon" class="absolute right-4 top-4">
+	<Sun
+		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+	/>
+	<Moon
+		class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+	/>
+	<span class="sr-only">Toggle theme</span>
+</Button>
 
 <Game />
